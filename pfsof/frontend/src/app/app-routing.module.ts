cf91ce85cc9485f7083026d6b8767281c3f2f753
sofiamarancibia <99927895+sofiamarancibia@users.noreply.github.com>
2022-09-25 12:15:08 -0300

    LOGINCOMPLETO

--- pfsof/frontend/src/app/app-routing.module.ts
+++ pfsof/frontend/src/app/app-routing.module.ts
@@ -1,7 +1,12 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { NgCircleProgressModule } from 'ng-circle-progress';
-const routes: Routes = [];
+import { HomeComponent } from './components/home/home.component';
+import { LoginComponent } from './components/login/login.component';
+const routes: Routes = [
+  {path: '', component: HomeComponent}, 
+  {path: 'login', component: LoginComponent},
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes), NgCircleProgressModule.forRoot({}) ],
