8205d2bba6372aece6d6063a3ab53f67fddefe62
sofiamarancibia <99927895+sofiamarancibia@users.noreply.github.com>
2022-10-12 15:45:56 -0300

    confcors

--- pfsof/backend/src/main/java/com/portfolio/mgb/security/MainSecurity.java
+++ pfsof/backend/src/main/java/com/portfolio/mgb/security/MainSecurity.java
@@ -3,10 +3,13 @@ package com.portfolio.mgb.security;
 import com.portfolio.mgb.security.Service.UserDetailsImpl;
 import com.portfolio.mgb.security.jwt.JwtEntryPoint;
 import com.portfolio.mgb.security.jwt.JwtTokenFilter;
+import java.time.Duration;
+import java.util.Arrays;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import static org.springframework.security.config.Customizer.withDefaults;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -16,6 +19,9 @@ import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.CorsConfigurationSource;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
 
 @Configuration
 @EnableWebSecurity
@@ -36,22 +42,33 @@ public class MainSecurity extends WebSecurityConfigurerAdapter {
         return new BCryptPasswordEncoder();
     }
 
-    @Override
+    @Bean
+ CorsConfigurationSource corsConfigurationSource() {
+  CorsConfiguration cc = new CorsConfiguration();
+                cc.setAllowedHeaders(Arrays.asList("Origin,Accept", "X-Requested-With", "Content-Type", "Access-Control-Request-Method", "Access-Control-Request-Headers","Authorization"));
+                cc.setExposedHeaders(Arrays.asList("Access-Control-Allow-Origin", "Access-Control-Allow-Credentials"));                
+  cc.setAllowedOrigins(Arrays.asList("/*"));
+  cc.setAllowedMethods(Arrays.asList("GET", "POST", "OPTIONS", "PUT","PATCH"));
+                cc.addAllowedOrigin("*");
+                cc.setMaxAge(Duration.ZERO);
+                cc.setAllowCredentials(Boolean.TRUE);
+  UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+  source.registerCorsConfiguration("/**", cc);
+  return source;
+ }
+    
+      @Override
     protected void configure(HttpSecurity http) throws Exception {
-
-        http.cors().and().csrf().disable()
+        http.cors(withDefaults()).csrf().disable()
                 .authorizeRequests()
                 .antMatchers("**").permitAll()
-                .anyRequest()
-                .authenticated()
+                .anyRequest().authenticated()
                 .and()
-                .exceptionHandling()
-                .authenticationEntryPoint(jwtEntryPoint)
+                .exceptionHandling().authenticationEntryPoint(jwtEntryPoint)
                 .and()
-                .sessionManagement()
-                .sessionCreationPolicy(SessionCreationPolicy.STATELESS);
-                http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);
-                
+                .sessionManagement().sessionCreationPolicy(SessionCreationPolicy.STATELESS);
+        http.addFilterBefore(jwtTokenFilter(), UsernamePasswordAuthenticationFilter.class);           
+    
 
     }
 
