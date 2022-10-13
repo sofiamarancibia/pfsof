b936d9be11f2f13118cd4aea9524a86c85d5bfc2
sofiamarancibia <99927895+sofiamarancibia@users.noreply.github.com>
2022-10-13 01:58:31 -0300

    gua

--- pfsof/frontend/src/app/app.module.ts
+++ pfsof/frontend/src/app/app.module.ts
@@ -17,8 +17,9 @@ import { FooterComponent } from './components/footer/footer.component';
 import {HttpClientModule} from '@angular/common/http';
 import { HomeComponent } from './components/home/home.component';
 import { LoginComponent } from './components/login/login.component';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { interceptorProvider } from './service/interceptor-service';
+
 
 @NgModule({
   declarations: [
@@ -41,6 +42,7 @@ import { interceptorProvider } from './service/interceptor-service';
     AppRoutingModule, 
     NgCircleProgressModule.forRoot({}),
     FormsModule,
+    ReactiveFormsModule,
     HttpClientModule
   ],
   providers: [interceptorProvider],
